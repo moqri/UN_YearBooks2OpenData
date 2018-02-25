68c92447c4c54f4f7af5703d527281c83049ae99
moqri <moqri@ufl.edu>
2018-02-25 11:36:50 -0500

    .

---
+++ run.sh
@@ -0,0 +1,3 @@
+python scripts/get_tables.py 
+java -jar open_source_tools/tabula-1.0.1-jar-with-dependencies.jar -b 3_table_pdfs/ -g
+mv 3_table_pdfs/*csv 4_table_csvs/
--- scripts/get_tables.py
+++ scripts/get_tables.py
@@ -1,7 +1,10 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
 import csv
-pdf_file = PdfFileReader(file("syb56.pdf", "rb"))
-with open('tables.csv', 'rb') as f:
+yearbook="1_yearbooks/syb56.pdf"
+table_pages="2_table_page_numbers/tables.csv"
+output_folder="3_table_pdfs/"
+pdf_file = PdfFileReader(file(yearbook, "rb"))
+with open(table_pages, 'rb') as f:
     reader = csv.reader(f)
     tables = list(reader)
 for table in tables[1:]:
@@ -10,6 +13,6 @@ for table in tables[1:]:
     output = PdfFileWriter()
     for page in range(begin-1,end):
         output.addPage(pdf_file.getPage(page))
-    outputStream = file(table_number+".pdf", "wb")
+    outputStream = file(output_folder+table_number+".pdf", "wb")
     output.write(outputStream)
     outputStream.close()
